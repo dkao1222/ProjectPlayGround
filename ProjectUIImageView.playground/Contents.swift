ee24cdfff6448046e0adb5867ceac91091890d46
Dragon Kao <dkao1222@gmail.com>
2016-05-11 13:36:12 +0800

    Project UIImageView

---
+++ ProjectUIImageView.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// set image name
+let image = UIImage(named: "swift-og.png")
+
+// set image shown image View
+let imageView = UIImageView(image: image)
+
